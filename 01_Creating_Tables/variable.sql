7d43748e10929b03dbc8f8d390e9dd0e3bbde3b5
Apurv <apurv.bmsit@gmail.com>
2021-12-17 17:34:21 +0530

    feat: variable in SQL

---
+++ 01_Creating_Tables/variable.sql
@@ -0,0 +1,12 @@
+INT -- whole number
+INTEGER -- whole number
+
+DECIMAL(m,n) -- decimal number with m digits before the decimal and n digits after the decimal
+
+VARCHAR(n) -- string of length n
+
+BLOB -- binary large object (binary large file)
+
+DATE -- date (year, month, day) 'YYYY-MM-DD'
+TIME -- time (hour, minute, second) 'HH:MM:SS'
+TIMESTAMP -- date and time 'YYYY-MM-DD HH:MM:SS'
